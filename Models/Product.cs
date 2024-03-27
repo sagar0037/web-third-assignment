33b00fabe7553ecd42934b2ef77ad57e078b7c22
Sagar Parajuli <sagarparajuli3030@gmail.com>
2024-03-27 15:53:51 -0400

    initial commit

---
+++ Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Assignment03.Models
+{
+    public class Product
+    {
+        public int ProductId { set; get; }
+        public string? Description { set; get; }
+        public string? Image { set; get; }
+        public double Pricing { set; get; }
+        public double ShippingCost { set; get; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace Assignment03.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? UserName { get; set; }
+        public string? PurchaseHistory { get; set; }
+        public string? ShippingAddress { get; set; }
+
+      
+    }
+}
